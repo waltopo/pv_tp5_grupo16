1cbc9a99b2f17446bea5abbd95309272914ad27e
MamaniErick <mamanierick64@gmail.com>
2025-06-05 20:20:51 -0300

    Reordenamiento de Archivos

--- demo-react-router/src/data/alumnoData.js
+++
@@ -1,8 +0,0 @@
-export const cargarAlumnos = () => {
-    const datos = localStorage.getItem('alumnosData');
-    return datos ? JSON.parse(datos) : [];
-};
-
-export const guardarAlumnos = (alumnos) => {
-    localStorage.setItem('alumnosData', JSON.stringify(alumnos));
-};
