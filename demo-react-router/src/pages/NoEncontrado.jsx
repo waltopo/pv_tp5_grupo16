f27e4161c1c6826ed322315954c99c3723fdd59d
MamaniErick <mamanierick64@gmail.com>
2025-06-05 16:46:38 -0300

    Desarrollo de Codigo[NoEncontrado.jsx][MamaniErick]

---
+++ demo-react-router/src/pages/NoEncontrado.jsx
@@ -0,0 +1,12 @@
+
+function NoEncontrado() {
+  return (
+    <div>
+      <h1>ERROR</h1>
+      <h2>Página no encontrada</h2>
+      <p>La ruta que estás buscando no existe.</p>
+    </div>
+  );
+};
+
+export default NoEncontrado;
