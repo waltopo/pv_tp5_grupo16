8529e54297dce36ce3922d9c5a0ec7cdbd9394c1
MamaniErick <mamanierick64@gmail.com>
2025-06-05 16:37:16 -0300

    Correcion de parrafos[AcercaDe.jsx][MamaniErick]

--- demo-react-router/src/pages/AcercaDe.jsx
+++ demo-react-router/src/pages/AcercaDe.jsx
@@ -2,9 +2,10 @@ export default function AcercaDe() {
   return (
     <div>
       <h2>Acerca del Desarrollo del Proyecto</h2>
-      <p>Aplicación desarrollada por el Grupo 16 de la Comision 1 de la asigantura Programacion 
-        Visual como trabajo práctico de React. El objetivo de la aplicacion es gestionar informacion de alumnos, permitir al usuario realizar las operaciones
-        basicas para una gestion de datos como lo es, crear, leer, editar y eliminar</p>
+      <p>Aplicación desarrollada por el Grupo 16 de la Comision 1 de la asignatura Programacion 
+        Visual como trabajo práctico de React.</p>
+      <p>El objetivo de la aplicacion es gestionar la informacion de alumnos, permitir al usuario realizar las operaciones
+        basicas para una gestion de datos como lo es: crear, leer, editar y eliminar</p>
       <p>La aplicación está estructurada en componentes funcionales y hace uso de React Router DOM para la navegación entre distintas vistas.</p>
       <p>Integrantes:</p>
       <ul>
