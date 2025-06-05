577839d2c2309963e9bc2685ce5dc5fe8b068c90
MamaniErick <mamanierick64@gmail.com>
2025-06-05 16:19:07 -0300

    Importado de archivo[App.jsx][MamaniErick]

--- demo-react-router/src/App.jsx
+++ demo-react-router/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react';
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0)
